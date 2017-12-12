9294e688ec877d709928f3b7c3917f82bc411d78
riccardomodanese <riccardo.modanese@eurotech.com>
2017-12-12 10:35:37 +0100

    fix security plugin - broker filter move logic outside security plugin (initial work)
    Signed-off-by: riccardomodanese <riccardo.modanese@eurotech.com>

--- broker-core/src/main/java/org/eclipse/kapua/broker/core/plugin/authentication/AdminAuthenticationLogic.java
+++ broker-core/src/main/java/org/eclipse/kapua/broker/core/plugin/authentication/AdminAuthenticationLogic.java
@@ -14,18 +14,31 @@ package org.eclipse.kapua.broker.core.plugin.authentication;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.kapua.KapuaException;
 import org.eclipse.kapua.broker.core.plugin.Acl;
-import org.eclipse.kapua.broker.core.plugin.AclConstants;
 import org.eclipse.kapua.broker.core.plugin.KapuaConnectionContext;
 
 public class AdminAuthenticationLogic extends AuthenticationLogic {
 
-    public List<AuthorizationEntry> buildAuthorizationMap(KapuaConnectionContext kcc) {
-        ArrayList<AuthorizationEntry> ael = new ArrayList<AuthorizationEntry>();
-        ael.add(createAuthorizationEntry(kcc, Acl.ALL, AclConstants.ACL_HASH));
-        ael.add(createAuthorizationEntry(kcc, Acl.WRITE_ADMIN, AclConstants.ACL_AMQ_ADVISORY));
+    public AdminAuthenticationLogic(String addressPrefix, String addressClassifier, String advisoryPrefix) {
+        super(addressPrefix, addressClassifier, advisoryPrefix);
+    }
 
+    @Override
+    public List<AuthorizationEntry> connect(KapuaConnectionContext kcc, AuthenticationCallback authenticationCallback) throws KapuaException {
+        return buildAuthorizationMap(kcc);
+    }
+
+    @Override
+    public void disconnect(KapuaConnectionContext kcc, AuthenticationCallback authenticationCallback, Throwable error) {
+    }
+
+    protected List<AuthorizationEntry> buildAuthorizationMap(KapuaConnectionContext kcc) {
+        ArrayList<AuthorizationEntry> ael = new ArrayList<AuthorizationEntry>();
+        ael.add(createAuthorizationEntry(kcc, Acl.ALL, aclHash));
+        ael.add(createAuthorizationEntry(kcc, Acl.WRITE_ADMIN, aclAdvisory));
         kcc.logAuthDestinationToLog();
         return ael;
     }
+
 }
